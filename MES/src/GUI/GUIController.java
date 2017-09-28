a2c2b5dab88f845551a4a8982ca7058fd6587545
alslevjonas <alslevjonas@gmail.com>
2017-09-28 15:24:04 +0200

    3orderdtodao: Auto stash before merge of "3orderdtodao" and "origin/3orderdtodao"

--- MES/src/GUI/GUIController.java
+++ MES/src/GUI/GUIController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mes;
+package GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author Troels
  */
-public class FXMLDocumentController implements Initializable {
+public class GUIController implements Initializable {
     
     @FXML
     private Label label;
