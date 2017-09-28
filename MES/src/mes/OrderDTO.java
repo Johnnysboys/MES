ff70721f9326fecec237dbd758e7fc20acb20544
Unknown <troels101@gmail.com>
2017-09-28 14:09:58 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ MES/src/mes/OrderDTO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Troels
+ */
+public class OrderDTO {
+    private int orderNumber;
+    private int articleNumber;
+    private int quantity;
+    private Date orderedFor;
+    private Date toBeDeliveredOn;
+    private OrderStatus status;
+   
+
+
+public OrderDTO(int orderNumber, int articleNumber, int quantity, Date orderedFor){
+    orderNumber = this.orderNumber;
+    articleNumber = this.articleNumber;
+    quantity = this.quantity;
+    orderedFor = this.orderedFor;
+    orderedFor = this.toBeDeliveredOn;
+    this.status = OrderStatus.UNSCHEDULED;
+    
+}
+}
