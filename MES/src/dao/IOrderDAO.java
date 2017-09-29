d94e1c79444aca3cc620e4b9c52758f4315ced44
Unknown <andersapple@live.dk>
2017-09-29 13:21:04 +0200

    3orderdtodao: Auto stash before merge of "3orderdtodao" and "origin/3orderdtodao"

--- MES/src/dao/IOrderDAO.java
+++ MES/src/dao/IOrderDAO.java
@@ -7,12 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface IOrderDAO {
-<<<<<<< Updated upstream
-    OrderDTO getOrder(String productionID, IConnector connection);
-    void updateOrder(OrderDTO order, IConnector connection);
-    ArrayList<OrderDTO> getAllOrders(IConnector connection);
-=======
     OrderDTO getOrder(String productionID, IERPConnector connection);
     void updateOrder(OrderDTO order, IERPConnector connection);
->>>>>>> Stashed changes
 }
