14f8a957ccf171a2024ae64de3b550862d5b008a
alslevjonas <alslevjonas@gmail.com>
2017-10-25 12:07:51 +0200

    List for Gui are set

--- MES/src/dao/OrderDAO.java
+++ MES/src/dao/OrderDAO.java
@@ -91,4 +91,23 @@ public class OrderDAO implements IOrderDAO{
         }
         return new ArrayList<>(orders.values());
     }
+    
+    public ArrayList getAllArticleNumber(){
+        ArrayList list = new ArrayList();
+        String query = getStatement("allArticleNumbers");
+        ResultSet res;
+        try {
+            connection.connectToDatabase();
+            res=connection.doQuery(query);
+            connection.closeConnection();
+            while(res.next()){
+                list.add(res);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+        
+    }
+    
 }
