3464cb75a697c4c04bc6aea2713e745cb4751a3d
Simon Sinding <sinding2000@gmail.com>
2017-11-01 21:38:51 +0100

    Added data object

---
+++ src/main/java/ORM/DataConverter.java
@@ -0,0 +1,22 @@
+package ORM;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataConverter {
+    public static List<Data> covertResultSetToData(ResultSet rs, Model model) {
+        ArrayList<Data> list = new ArrayList<>();
+        try {
+            while(rs.next()) {
+                for (Definition definition : model.getDefinitions()) {
+                    list.add(new Data(definition.getFieldName(), rs.getObject(definition.getFieldName())));
+                }
+            }
+        } catch (SQLException e) {
+            // We don't care if it does not exists the result set... Naaaaah fuck that shit..
+        }
+        return list;
+    }
+}
