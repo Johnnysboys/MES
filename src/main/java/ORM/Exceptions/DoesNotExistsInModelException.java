6505e89d1744ede18988d8e222a207924d936859
Simon Sinding <sinding2000@gmail.com>
2017-11-01 21:39:33 +0100

    Added exceptions

---
+++ src/main/java/ORM/Exceptions/DoesNotExistsInModelException.java
@@ -0,0 +1,11 @@
+package ORM.Exceptions;
+
+public class DoesNotExistsInModelException extends Exception {
+    public DoesNotExistsInModelException(){
+        super("Field does not exists in the model");
+    }
+
+    public DoesNotExistsInModelException(String s) {
+        super(s);
+    }
+}
