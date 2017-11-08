f761c8d5e89ebb5fbf0b5add904aadf923429a33
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-11-08 11:11:12 +0100

    testing dao

--- src/main/java/dao_mes/DAOMAIN.java
+++ src/main/java/dao_mes/DAOMAIN.java
@@ -8,7 +8,9 @@ public class DAOMAIN {
     public static void main(String[] args) {
         OrderDAO.get();
         List<OrderDTO> list = OrderDAO.get().getAllOrders();
-        for(OrderDTO d:list)
+        for(OrderDTO d:list) {
             System.out.println(d.getOrderNumber());
+            System.out.println(d.getArticleNumber());
+        }
     }
 }
