9f832ec7a6c91cc6e7fdd648a6c47a44b6deb0c5
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-11-23 10:13:05 +0100

    damn boi

--- src/main/java/scadaConnection/RMIServer.java
+++ src/main/java/scadaConnection/RMIServer.java
@@ -1,5 +1,4 @@
-package main.java.scadaConnection;
-
+package scadaConnection;
 import dao_mes.OrderDAO;
 import dto_mes.OrderDTO;
 import dto.OrderINFO;
@@ -7,8 +6,6 @@ import dto_mes.OrderStatus;
 import mes.AbstractMES;
 import mes.RMI_Constants;
 import mes.ExceedsCapacityException;
-import mes.IMESServer;
-import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -17,17 +14,16 @@ public class RMIServer extends AbstractMES{
 
     public RMIServer() throws RemoteException {
         super();
-        try {
-            Registry registry = LocateRegistry.createRegistry(RMI_Constants.MES_PORT);
-            IMESServer mes = this;
-            registry.bind(RMI_Constants.MES_OBJECTNAME, mes);
-        } catch (AlreadyBoundException e) {
-            e.printStackTrace();
-        }
+        //Uses shared constants from the MESCADAPI.jar library.
+        Registry registry = LocateRegistry.createRegistry(RMI_Constants.MES_PORT);
+        registry.rebind(RMI_Constants.MES_OBJECTNAME, this);
+
     }
 
     @Override
     public void alertPlanted(String orderID) throws RemoteException {
+        //Starts new thread which sends commands to the OrderDAO.
+        //It is possible that the rmi server starts threads implicitly, but it should always start a new thread.
         new Thread(()->{
             OrderDTO temp =OrderDAO.get().getOrder(orderID);
             temp.addPlanted(1);
@@ -60,14 +56,12 @@ public class RMIServer extends AbstractMES{
         }).start();
     }
 
-    public void executeOrder(OrderDTO orderDTO) throws RemoteException {
+    public void executeOrder(OrderDTO orderDTO) throws RemoteException, ExceedsCapacityException {
+        //Method evaluates which status the order is in, and if it is to be executed, it executes it using the
+        //method in the super class, AbstractMES, found in MESCADAPI.jar
         if(orderDTO.getStatus().equals(OrderStatus.UNSCHEDULED)){
             orderDTO.setStatus(OrderStatus.SCHEDULED);
-            try {
-                super.executeOrder(new OrderINFO(orderDTO.getArticleNumber(),orderDTO.getToBePlanted(),orderDTO.getOrderNumber()));
-            } catch (ExceedsCapacityException e) {
-                return;
-            }
+            super.executeOrder(new OrderINFO(orderDTO.getArticleNumber(),orderDTO.getToBePlanted(),orderDTO.getOrderNumber()));
             orderDTO.setStatus(OrderStatus.IN_PRODUCTION);
         }
     }
