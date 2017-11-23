10f074d4a806b33931de5f31b15abb774e3838ba
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-11-23 12:00:54 +0100

    boom, ny importer den rigtigt

--- src/main/java/GUI/GUIController.java
+++ src/main/java/GUI/GUIController.java
@@ -7,24 +7,20 @@ package GUI;
 
 import dto_mes.OrderDTO;
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import head.MESController;
-import java.rmi.RemoteException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.event.Event;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import mes.ExceedsCapacityException;
+import scadaConnection.RMIServer;
 
 /**
  *
@@ -71,7 +67,7 @@ public class GUIController implements Initializable {
         
           }
     
-    private void handleExecute(ActionEvent event, RMIServer rmi, MESController mc){
+    private void handleExecute(ActionEvent event, RMIServer rmi, MESController mc) throws ExceedsCapacityException, RemoteException {
         rmi.executeOrder((OrderDTO) mc.getERPOrderList());
         OrderTable.refresh();
         ERPTable.refresh();
